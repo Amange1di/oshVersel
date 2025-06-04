68604654e8e421dc4f37cf4f2a920c16de3a376c
Amange1di <amangeldi2702006@gmail.com>
2025-06-04 18:41:32 +0600

    dsd

--- src/app/redux/slices/gellarySlice.js
+++ src/app/redux/slices/gellarySlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import instance from '../../../../shared/api/axios.js';
+import instance from '@shared/api/axios.js';
 
 export const fetchGalleryPhotos = createAsyncThunk(
     'gallery/fetchGalleryPhotos',
